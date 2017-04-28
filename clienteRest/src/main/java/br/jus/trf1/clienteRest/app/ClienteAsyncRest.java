df11397a84e8814e4b27d4f873ce0dd4336e785d
tr300869 <tr300869@dipsi05-trf1.trf1.gov.br>
2017-04-28 14:03:02 -0300

    nova versão

--- clienteRest/src/main/java/br/jus/trf1/clienteRest/app/ClienteAsyncRest.java
+++ clienteRest/src/main/java/br/jus/trf1/clienteRest/app/ClienteAsyncRest.java
@@ -14,12 +14,14 @@ import br.jus.trf1.pocArquitetura.entidades.Funcionario;
 public class ClienteAsyncRest {
 
 	private static Future future;
+	private static int quantCompletas = 1;
+	private static int quantFalhas = 1;
 
 	public static void main(String[] args) throws InterruptedException {
 		
-		for (int i = 0; i < 50; i++) {
+		for (int i = 0; i < 800; i++) {
 			request();
-			Thread.sleep(new Random().nextInt(2000/50));
+			Thread.sleep(new Random().nextInt(94));
 		}
 		
 		
@@ -29,24 +31,24 @@ public class ClienteAsyncRest {
 	}
 
 	private static void request() {
-		Client client = ClientBuilder.newClient();
+		final Client client = ClientBuilder.newClient();
 		final WebTarget target = client.target("http://localhost:8080/arquitetura/rest/async/funcionario/2");
 		future = target.request().async().get(new InvocationCallback<Funcionario>() {
 
-			@Override
 			public void completed(final Funcionario arg0) {
 				System.out.println( LocalDateTime.now()+" - Completo "+ Thread.currentThread().getName());
 			      // target.request().async().get(this);
 				client.close();
+				System.out.println("Quantidade Completas: "+quantCompletas++);
 				
 			}
 
-			@Override
 			public void failed(final Throwable arg0) {
 				// TODO Auto-generated method stub
 				System.out.println(LocalDateTime.now()+" - falha "+ Thread.currentThread().getName());
 				arg0.printStackTrace();
 				client.close();
+				System.out.println("Quantidade Falhas: "+quantFalhas++);
 			}
 			
 		});
